a04e3e3f3b9ec57568a314f19e498fbc43b600a8
kulsgam <kuls.gam@gmail.com>
2024-07-19 00:01:20 +1000

    Reordered css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Theme } from "@radix-ui/themes";
 import { useAppearance } from "@/contexts/appearance";
 import {
